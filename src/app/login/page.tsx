a72c46a1e070342ca5884f923cdb4d24e6fa7f99
DvlPrSujalPatel <sujal7455@gmail.com>
2024-10-20 21:29:26 +0530

    ui change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,10 +14,14 @@ export default function LoginPage(){
 
     }
 
-    return <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <h1 className="text-3xl text-white">Login</h1>
-        <hr />
-        <label htmlFor="email">Email</label>
+    return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <div className="mb-8">
+        <h1 className="text-3xl mb-8">Login Here</h1>
+        </div>
+      <div className="w-1/2 p-4">
+      <div className="flex gap-1 flex-col">
+       <label htmlFor="email">Email</label>
         <input 
         className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         type="email"
@@ -26,20 +30,28 @@ export default function LoginPage(){
         onChange={(e) => setUser({...user, email: e.target.value})}
         placeholder="Email"
         />
+       </div>
+        <div className="flex gap-1  flex-col">
         <label htmlFor="password">Password</label>
         <input 
-        className="p-2 border  text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border  text-black border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
         type="password"
         id="password"
         value={user.password}
         onChange={(e) => setUser({...user, password: e.target.value})}
         placeholder="Password"
         />
+        </div>
+        <div className="flex mb-4 justify-between gap-1">
+        <Link className="text-blue-600 text-sm mt-1 block" href="/signup">Forgot Password?</Link>
+        <Link className="text-[#a8dadc] text-sm mt-1 block" href="/signup">Don't Have a Account?Signup here</Link>
+        </div>
         <button 
         onClick={onLogin}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        className="py-2 px-4 hover:bg-[#a51b1b]  bg-[#e63946] text-lg border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 w-full">
             Login here
         </button>
-        <Link href="/signup">Don't Have a Account?</Link>
+      </div>
     </div>
+    )
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,9 +16,10 @@ export default function SignUpPage(){
     }
 
     return <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <h1 className="text-3xl text-white">Sign UP</h1>
-        <hr />
-        <label htmlFor="username">Username</label>
+        <h1 className="text-3xl mb-8">Signup Here</h1>
+        <div className="w-1/2 p-4">
+       <div className="flex gap-1 flex-col">
+       <label htmlFor="username">Username</label>
         <input 
         className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         type="text"
@@ -27,6 +28,8 @@ export default function SignUpPage(){
         onChange={(e) => setUser({...user, username: e.target.value})}
         placeholder="username"
         />
+       </div>
+        <div className="flex gap-1 flex-col">
         <label htmlFor="email">Email</label>
         <input 
         className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
@@ -36,20 +39,24 @@ export default function SignUpPage(){
         onChange={(e) => setUser({...user, email: e.target.value})}
         placeholder="Email"
         />
+        </div>
+        <div className="flex gap-1 flex-col">
         <label htmlFor="password">Password</label>
         <input 
-        className="p-2 border  text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border  text-black border-gray-300 rounded-lg  focus:outline-none focus:border-gray-600"
         type="password"
         id="password"
         value={user.password}
         onChange={(e) => setUser({...user, password: e.target.value})}
         placeholder="Password"
         />
+        </div>
+        <Link className="text-[#a8dadc] text-sm mt-1 block" href="/login">Already registered?</Link>
         <button 
         onClick={onSignup}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        className="py-2 px-4 hover:bg-[#a51b1b]  bg-[#e63946] text-lg border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 w-full mt-4">
             Signup here
         </button>
-        <Link href="/login">Already registered?</Link>
+        </div>
     </div>
 };
